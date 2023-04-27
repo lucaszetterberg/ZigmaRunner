8eee9a03a6748db814ff28800b5d422e9ca52206
lucasz <lucasz@kth.se>
2023-04-27 19:41:08 +0200

    add slidefunction

--- src/ZRMain.py
+++ src/ZRMain.py
@@ -24,7 +24,7 @@ SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 RUNNING = [pygame.image.load(os.path.join("images", "ZigmaRun1.png")), pygame.image.load(os.path.join("images", "ZigmaRun2.png"))]
 JUMPING = pygame.image.load(os.path.join("images", "ZigmaJump.png"))
-DUCKING = [pygame.image.load(os.path.join("images", "LargeCactus1.png"))]
+SLIDING = [pygame.image.load(os.path.join("images", "ZigmaSlide.png"))]
 
 SMALL_CACTUS = [pygame.image.load(os.path.join("images", "LargeCactus1.png"))]
 LARGE_CACTUS = [pygame.image.load(os.path.join("images", "LargeCactus1.png"))]
@@ -36,13 +36,14 @@ font = pygame.font.Font("freesansbold.ttf",16)
 class Player:
     PLAYER_X = 80
     PLAYER_Y = 310
+    PLAYER_Y_SLIDE = 340
 
     def __init__(self):
-        self.duck_img = DUCKING
+        self.slide_img = SLIDING
         self.run_img = RUNNING
         self.jump_img = JUMPING
 
-        self.player_duck = False
+        self.player_slide = False
         self.player_run = True
         self.player_jump = False
 
@@ -53,32 +54,35 @@ class Player:
         self.player_rect.y = self.PLAYER_Y
 
     def update(self, keyboardInput):
-        if self.player_duck:
-            self.duck()
+        if self.player_slide:
+            self.slide()
         if self.player_run:
             self.run()
         if self.player_jump:
             self.jump()
-
         if self.step_index >= 10:
             self.step_index = 0
 
         if keyboardInput[pygame.K_UP] or keyboardInput[pygame.K_SPACE] and not self.player_jump:
-            self.player_duck = False
+            self.player_slide = False
             self.player_run = False
             self.player_jump = True
         elif keyboardInput[pygame.K_DOWN] and not self.player_jump:
-            self.player_duck = True
+            self.player_slide = True
             self.player_run = False
             self.player_jump = False
         elif not (self.player_jump or keyboardInput[pygame.K_DOWN]):
-            self.player_duck = False
+            self.player_slide = False
             self.player_run = True
             self.player_jump = False
 
-    def duck(self):
-       pass
-
+    def slide(self):
+        self.image = self.slide_img[0]
+        self.player_rect = self.image.get_rect()
+        self.player_rect.x = self.PLAYER_X
+        self.player_rect.y = self.PLAYER_Y_SLIDE
+        self.step_index += 1
+        
     def run(self):
         self.image = self.run_img[self.step_index // 5]
         self.player_rect = self.image.get_rect()
