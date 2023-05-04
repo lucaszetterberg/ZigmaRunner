50044065855e2bb70af096de0c71a63c611f3c8c
Dilan Saleh <dilansaleh@n139-p45.eduroam.kth.se>
2023-05-04 15:54:06 +0200

    Fix

--- src/ZRMain.py
+++ src/ZRMain.py
@@ -147,8 +147,8 @@ class LargeCactus(Obstacle):
         self.type = random.randint(0,2)
         super().__init__(image, self.type)
         self.rect.y = 420
-
-
+        
+        
 
 class Cloud:
     def __init__(self):
