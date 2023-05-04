0870ade0289d9f6bf8d355c7437b87863f0ca806
lucasz <lucasz@kth.se>
2023-05-05 00:49:51 +0200

    Move player class to separate file

--- src/ZRMain.py
+++ src/ZRMain.py
@@ -1,6 +1,7 @@
 import pygame, random, os, sys
 from constants import *
 from menu import menu
+from player import Player
 
 pygame.init()
 
@@ -17,80 +18,6 @@ pygame.display.set_caption("Zigma runner")
 obstacles = []
 game_over = False
 
-class Player:
-    PLAYER_X = 80
-    PLAYER_Y = 430
-    PLAYER_Y_SLIDE = 470
-    JUMP_VEL = 8.5
-
-    def __init__(self):
-        self.slide_img = SLIDING
-        self.run_img = RUNNING
-        self.jump_img = JUMPING
-
-        self.player_slide = False
-        self.player_run = True
-        self.player_jump = False
-
-        self.step_index = 0
-        self.image = self.run_img[0]
-        self.player_rect = self.image.get_rect()
-        self.player_rect.x = self.PLAYER_X
-        self.player_rect.y = self.PLAYER_Y
-        self.jump_vel = self.JUMP_VEL
-
-    def update(self, keyboardInput):
-        if self.player_slide:
-            self.slide()
-        if self.player_run:
-            self.run()
-        if self.player_jump:
-            self.jump()
-        if self.step_index >= 10:
-            self.step_index = 0
-
-        ##if not game_over:
-        if keyboardInput[pygame.K_UP] or keyboardInput[pygame.K_SPACE] and not self.player_jump:
-            self.player_slide = False
-            self.player_run = False
-            self.player_jump = True
-        elif keyboardInput[pygame.K_DOWN] and not self.player_jump:
-            self.player_slide = True
-            self.player_run = False
-            self.player_jump = False
-        elif not (self.player_jump or keyboardInput[pygame.K_DOWN]):
-            self.player_slide = False
-            self.player_run = True
-            self.player_jump = False  
-
-    def slide(self):
-        self.image = self.slide_img[0]
-        self.player_rect = self.image.get_rect()
-        self.player_rect.x = self.PLAYER_X
-        self.player_rect.y = self.PLAYER_Y_SLIDE
-        self.step_index += 1
-        
-    def run(self):
-        self.image = self.run_img[self.step_index // 5]
-        self.player_rect = self.image.get_rect()
-        self.player_rect.x = self.PLAYER_X
-        self.player_rect.y = self.PLAYER_Y
-        if not game_over_game:
-            self.step_index += 1
-
-    def jump(self):
-        self.image = self.jump_img
-        if self.player_jump:
-            self.player_rect.y -= int(self.jump_vel * 4)
-            self.jump_vel -= 0.8
-        if self.jump_vel < -self.JUMP_VEL:
-            self.player_jump = False
-            self.jump_vel = self.JUMP_VEL
-
-
-    def draw(self, SCREEN):
-        SCREEN.blit(self.image, (self.player_rect.x, self.player_rect.y))
-    
 class Obstacle:
     def __init__(self, image, type):
         self.image
@@ -183,7 +110,7 @@ def Game(LARGE_OBSTACLES):
     player = Player()
     
     ## Main game loop, can be viewed as what is happening in each frame
-    pygame.event.clear()
+    ##pygame.event.clear()
     while game_running:
         timer.tick(fps)
         SCREEN.fill((white))
@@ -233,7 +160,5 @@ def main():
             if Delan:
                 game_state = menu(game_over=True)
         
-            
-    
-    
+
 main()
